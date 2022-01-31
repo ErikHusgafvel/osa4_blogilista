5d21d57b819c4c7dad9ad52c6d930faf2dc1ac79
Erik Husgafvel <husgafvel.erik@gmail.com>
2022-01-31 15:53:02 +0200

    4.2: blogilista, step2 index.js & app.js

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-require('dotenv').config()
 //const http = require('http')
-const express = require('express')
-const app = express()
+const app = require('./app')
 const config = require('./utils/config')
-const cors = require('cors')
-const notesRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', notesRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
