b4afa5dcbce70081f693bedb72e0bef57c20dc4a
Erik Husgafvel <husgafvel.erik@gmail.com>
2022-02-01 16:00:07 +0200

    4.5*: apufunktioita ja yksikkötestejä, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,21 @@ const totalLikes = (blogs) => {
     ? 0 : blogs.reduce((sum, blog) => (sum + blog.likes), 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0 ? {} :
+    blogs.reduce((currentBlog, newBlog) => {
+      return currentBlog.likes > newBlog.likes
+        ? currentBlog :
+        {
+          title: newBlog.title,
+          author: newBlog.author,
+          likes: newBlog.likes
+        }
+    }, {})
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
