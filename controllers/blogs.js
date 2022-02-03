c17d299d4ce05953ebe028cdbda32b5b6c3915bc
Erik Husgafvel <husgafvel.erik@gmail.com>
2022-02-03 12:29:18 +0200

    4.10: blogilistan testit, step3

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,21 @@
 const notesRouter = require('express').Router()
 const Blog = require('../models/blog.js')
 
-notesRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+notesRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-notesRouter.post('/', (request, response) => {
+notesRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch(exception) {
+    next(exception)
+  }
+
 })
 
 module.exports = notesRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,78 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+const initialBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+
+  /* Saving new blog objects to db could also have been done in either ways:
+  (1)
+  for (let blog of initialBlogs) { //for..of structure maintains the order (objects will be saved in same order as in the initialBlogs)
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+
+  (2)
+  await Blog.insertMany(initialBlogs)
+  */
+})
 
 test('blogs are returned as json', async () => {
   const response = await api
@@ -10,7 +80,7 @@ test('blogs are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
-  expect(response.body).toHaveLength(1)
+  expect(response.body).toHaveLength(initialBlogs.length)
 })
 
 test('blog has id field', async () => {
@@ -18,6 +88,47 @@ test('blog has id field', async () => {
     .get('/api/blogs')
 
   expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: '9 things most get wrong about usability testing – and how to fix them',
+    author: 'Karri-Pekka Laakso',
+    url: 'https://www.reaktor.com/blog/9-things-most-get-wrong-about-usability-testing-and-how-to-fix-them/',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(blog => blog.title)
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+  expect(titles).toContain(
+    '9 things most get wrong about usability testing – and how to fix them'
+  )
+  /*
+  api
+    .post('/api/blogs').send(newBlog)
+    .then(() => {
+      expect(201)
+    })
+    .catch(err => console.log(err.message))
+
+  api
+    .get('/api/blogs')
+    .then(response => {
+      const titles = response.body.map(blog => blog.title)
+      expect(response.body).toHaveLength(initialBlogs.length + 1)
+      expect(titles).toContain(
+        '9 things most get wrong about usability testing – and how to fix them'
+      )
+    })
+    .catch(err => console.log(err.message))
+  */
 })
 
 afterAll(() => {
